dbc4579eec9e1196f5a514f7b5a38cb0cd274b65
Priyansh Neel <neelpriyansh@gmail.com>
2025-07-25 23:19:48 +0530

    first commit

--- my-expo-app/components/Header.tsx
+++ my-expo-app/components/Header.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 
 const Header = () => {
   return (
-    
+    <View className="flex-row items-center justify-between px-6 py-4">
+        <AntDesign name="arrowleft" size={28} color="white" />
+        <Feather name="bell" size={28} color="white" />  
+    </View> 
   )
 }
 
