414d3c6b77a885a91f71be118e5c4a9308aed72b
Priyansh Neel <neelpriyansh@gmail.com>
2025-07-25 23:28:17 +0530

    UI/UX

--- my-expo-app/App.tsx
+++ my-expo-app/App.tsx
@@ -10,7 +10,6 @@ export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-green-400">
       <StatusBar style="light" />
-      <Header/>
       {/* Profile */}
       {/* <View className="items-center mb-4">
         <Image
