65f16caba2b106f3fd33acec6a2b5e38165c423b
Priyansh Neel <neelpriyansh@gmail.com>
2025-07-29 23:07:43 +0530

    Start Now

--- my-expo-app/App.tsx
+++ my-expo-app/App.tsx
@@ -48,7 +48,7 @@ export default function App() {
         </Text>
         {/* Health Recommendations */}  
         <View className="flex-row justify-around mt-6"> 
-          <View className="items-center">
+          <View className="items-center"> 
             <Text className="mt-1 text-xs text-gray-600">Mask</Text> 
           </View>
           <View className="items-center">
