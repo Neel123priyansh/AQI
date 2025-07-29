8d064f17cee1e665cb52ee2e2d03d4d472d047e7
Priyansh Neel <neelpriyansh@gmail.com>
2025-07-29 23:05:07 +0530

    Start Now

--- my-expo-app/App.tsx
+++ my-expo-app/App.tsx
@@ -69,7 +69,7 @@ export default function App() {
         </Pressable> 
         <Pressable className="bg-gray-200 px-4 py-2 rounded-full">
           <Text className="text-gray-800 text-sm">AQI Daily Averages</Text> 
-        </Pressable>
+        </Pressable> 
       </View>
     </SafeAreaView>
   );
