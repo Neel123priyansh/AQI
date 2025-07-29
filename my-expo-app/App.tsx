9c73e1bfcda094c2577a916be0d4dab77cad6653
Priyansh Neel <neelpriyansh@gmail.com>
2025-07-29 23:04:25 +0530

    Start Now

--- my-expo-app/App.tsx
+++ my-expo-app/App.tsx
@@ -55,7 +55,7 @@ export default function App() {
             <Text className="mt-1 text-xs text-gray-600">Jog</Text>
           </View>
           <View className="items-center">
-            <Text className="mt-1 text-xs text-gray-600">Indoor</Text>
+            <Text className="mt-1 text-xs text-gray-600">Indoor</Text> 
           </View>
           <View className="items-center">
             <Text className="mt-1 text-xs text-gray-600">Purifier</Text>
