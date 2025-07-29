97b506769ddc4db7eaa8b9956a7f9185e65e956e
Priyansh Neel <neelpriyansh@gmail.com>
2025-07-29 23:07:22 +0530

    Start Now

--- my-expo-app/App.tsx
+++ my-expo-app/App.tsx
@@ -23,7 +23,7 @@ export default function App() {
       {/* AQI Card */}  
       <View className="bg-white mx-6 my-[2rem] rounded-3xl p-6"> 
         <Text className="text-gray-800 font-semibold mb-2"> 
-          📍 New Delhi, Indonesia 
+          📍 New Delhi, India
         </Text>
         <Text className="text-2xl font-bold text-red-500 mb-4">   
           Unhealthy
